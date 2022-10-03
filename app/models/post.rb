c275b2f9e0e7b6cea27e943788b63fed08f54e7d
Rose Maina <rosewairimu27@gmail.com>
2022-10-03 15:14:04 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']} 
+    validates :title, inclusion: {in: ["You Won't Believe These True Facts"]}
 end
